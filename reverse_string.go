9a24576dc00d4cc4904899183336fbedcf432ca3
Art83 <shvetsov1983@gmail.com>
2022-05-18 12:12:02 +1000

    first attempt

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,18 @@
 package reverse_string
 
+import (
+	"bufio"
+	"os"
+)
+
 func ReverseString(input string) (output string) {
-	// solution goes here
-	return output
+	scanner := bufio.NewScanner(os.Stdin)
+	if scanner.Scan() {
+		input = scanner.Text()
+	}
+	byte_arr := []rune(input)
+	for i, j := 0, len(byte_arr)-1; i < j; i, j = i+1, j-1 {
+		byte_arr[i], byte_arr[j] = byte_arr[j], byte_arr[i]
+	}
+	return string(byte_arr)
 }
